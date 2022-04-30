46e91eeb7a4d70bf66430415089fc97ca4d8f1ee
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2022-04-30 12:08:08 -0600

    cleanup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,4 +4,3 @@ import { BrowserRouter } from "react-router-dom";
 import { App } from './App'
 
 ReactDOM.render(<App/>, document.querySelector('#app'))
-// ReactDOM.render(<h1>Hello World</h1>, document.querySelector('#app'))
