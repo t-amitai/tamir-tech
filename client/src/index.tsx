602f77174c3bdd9e8b3eeda7fc1d5da9beeabf24
Tamir Amitai <tamitai147@gmail.com>
2022-04-30 12:06:20 -0600

    converts src to typescript and adapts webpack.config

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from "react-router-dom";
-import { App } from './App.jsx'
+import { App } from './App'
 
 ReactDOM.render(<App/>, document.querySelector('#app'))
+// ReactDOM.render(<h1>Hello World</h1>, document.querySelector('#app'))
