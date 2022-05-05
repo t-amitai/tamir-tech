fc1fba506db4aa0c8594c9058466646175f9b4c9
Tamir Amitai <tamitai147@gmail.com>
2022-05-05 06:48:40 -0600

    finishes 1st draft of PageNotFound

--- client/src/views/PageNotFound.tsx
+++ client/src/views/PageNotFound.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
 function PageNotFound() {
+    const page = window.location.href.split('/').pop()
+
     return(
-        <h1>Page Not Found</h1>
+        <>
+            <h1>404 Error: Page not found</h1>
+            <h5>The page you requested does not exist.</h5>
+            <h5>Page: {page}</h5>
+        </>
     )
 }
 
