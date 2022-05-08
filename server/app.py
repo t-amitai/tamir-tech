bb4fc6f7ba626667fa6a441c7404d9dd8994e5d1
Tamir Amitai <tamitai147@gmail.com>
2022-05-07 19:09:42 -0600

    server/app-Adds HangmanHandler

--- server/app.py
+++ server/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, escape, request, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from api.HangmanHandler import HangmanHandler
 
 
 app = Flask(__name__, static_url_path='', static_folder='./../client/dist')
+api = Api(app)
 
 
 @app.route("/", defaults={'path':''})
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
+
+api.add_resource(HangmanHandler)
