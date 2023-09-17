32b2df389d9fd1206314783b877d89950154a44e
Tamir Amitai <tamitai147@gmail.com>
2023-09-17 12:58:17 -0600

    cleanup

--- client/src/views/PageNotFound.tsx
+++ client/src/views/PageNotFound.tsx
@@ -6,8 +6,8 @@ function PageNotFound() {
     return(
         <>
             <h1>404 Error: Page not found</h1>
-            <h5>The page you requested does not exist.</h5>
-            <h5>Page: {page}</h5>
+            <h3>The page you requested does not exist.</h3>
+            <h3>Page: {page}</h3>
         </>
     )
 }
