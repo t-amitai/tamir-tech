c1efa0a8abcfd3785d04a1e6c4d6f7024360953e
Tamir Amitai <tamitai147@gmail.com>
2023-09-17 12:58:19 -0600

    refactor header to use navigation array

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-  
+
+const navigation = [
+    {name:'About', link:'about'},
+    {name:'Resume', link:'resume'},
+    {name:'Projects', link:'project'},
+]
 export default function Header() {
-return (
-    <nav>
-        <Link to='about'>About</Link>
-        <Link to='resume'>Resume</Link>
-    </nav>
-)
+    return (
+        <nav>
+            {navigation.map(nav => {
+                    return (
+                        <Link className="m-2" to={nav.link}> {nav.name} </Link>
+                    )
+                }
+            )}
+        </nav>
+    )
 }
