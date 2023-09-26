b6b8ecbdfc902148689d16b806f66604db4da559
Tamir Amitai <tamitai147@gmail.com>
2023-09-26 17:38:48 -0600

    adds responsive padding and margin to Header.jsx buttons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,26 +13,16 @@ export default function Header() {
     return (
         <nav>
             {navigation.map((nav, index) => {
-                if (index === 0) {
-                    return (
-                        <Link
-                            className="mx-2 my-2 lg:mx-4"
-                            to={nav.link}
-                            key={nav.link}
-                        >
-                            <div className="h-10bg-cover"
-                                 style={{
-                                     backgroundImage: `url(${iconPic})`,
-                                 }}/>
-                        </Link>
-                    )
-                }
                 return (
-                    <Link
-                        className="m-2 lg:m-4 hover:text-slate-600"
-                        to={nav.link}
-                        key={nav.link}
-                    > {nav.name} </Link>
+                    <Link to={nav.link} key={nav.link}>
+                        <button
+                            className="m-1 lg:m-2 xl:m-4 lg:mx-3 xl:mx-5 inline-flex items-center justify-center p-0.5 text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+                        >
+                            <span
+                                className="px-2 py-1 lg:px-4 lg:py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
+                            >{nav.name}</span>
+                        </button>
+                    </Link>
                 )}
             )}
         </nav>
