e29f6406c547f70dc24316427ece866f26cc0694
Tamir Amitai <tamitai147@gmail.com>
2023-09-30 17:51:27 -0600

    finishes home page

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,8 +1,22 @@
 import * as React from 'react'
 import slateLake from '../images/slateLake.jpg'
+import {Link} from "react-router-dom";
+import {BsFillHouseDoorFill} from "react-icons/bs";
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <div className='mx-auto max-w-7xl px-4 py-16 text-center sm:px-6 sm:py-24 lg:px-8 lg:py-48'>
+            <p className='text-base lg:text-xl font-bold text-black'>Hi, I'm Tamir!</p>
+            <h1 className='mt-2 text-2xl tracking-tight text-black sm:text-5xl'>A Software Engineer working in QA. </h1>
+            <p className='mt-2 text-lg font-medium text-black text-opacity-50'>
+                I'm using this website to showcase my skills.
+            </p>
+            <p className='mt-2 text-lg font-medium text-black text-opacity-50'>
+                Some features rely on a server, which may or may not be running.
+            </p>
+            <p className='mt-2 text-lg font-medium text-black'>
+                Please enjoy
+            </p>
+        </div>
     )
 }
