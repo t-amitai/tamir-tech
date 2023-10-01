241c1d7685788600ac659d3191465031714b2e76
Tamir Amitai <tamitai147@gmail.com>
2023-10-01 10:09:51 -0600

    fixes dev.py

--- server/dev.py
+++ server/dev.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_restful import Api
 
 app = Flask(__name__, static_url_path='', static_folder='./../client/dist')
 api = Api(app)
+
 
 @app.route("/", defaults={'path':''})
 def serve(path):
