d60a1d88203dad3e449c809984247ea8ff666951
Tamir Amitai <tamitai147@gmail.com>
2023-12-09 10:50:34 -0700

    updates app.py with only resume route

--- server/app.py
+++ server/app.py
@@ -1,13 +1,21 @@
-from flask import Flask, escape, request, send_from_directory
-from flask_restful import Api, Resource, reqparse
-from api.HangmanHandler import HangmanHandler
+from flask import Flask, request, abort
+from flask_restful import Api
+from json import loads
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
-app = Flask(__name__, static_url_path='', static_folder='./../client/dist')
+app = Flask(__name__)
 api = Api(app)
 
 
-@app.route("/", defaults={'path':''})
-def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
-
+@app.post('/resume')
+def resume():
+    body = loads(request.data)
+    if body['password'] == os.getenv('RESUME_PASSWORD'):
+        with open("assets/resume.txt") as f:
+            data = f.read()
+        return data
+    else:
+        abort(401)
