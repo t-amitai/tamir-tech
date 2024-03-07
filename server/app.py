f144f14314999550fe9ae5920c4a9efc8dbd2bd3
Tamir Amitai <tamitai147@gmail.com>
2024-03-07 09:17:50 -0700

    deletes dev.py

--- server/app.py
+++ server/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, abort
+from flask import Flask, send_from_directory, request, abort
 from flask_restful import Api
 from json import loads
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='./../client/dist')
 api = Api(app)
+
+
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
 
 
 @app.post('/resume')
@@ -19,3 +23,6 @@ def resume():
         return data
     else:
         abort(401)
+
+
+
