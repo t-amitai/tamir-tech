e335707a4542c5fc63581ad368ce12ae1e04eda9
Tamir Amitai <tamitai147@gmail.com>
2024-03-10 18:01:09 -0600

    contact page placeholder while backend development

--- client/src/views/Contact.jsx
+++ client/src/views/Contact.jsx
@@ -28,11 +28,13 @@ export default function Contact() {
     return(
         <section className="">
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                {
+                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Me</h2>
+                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">tamitai147@gmail.com</p>
+                {/* {
                     !sent ?
                     <>
-                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-                        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Me</h2>
+                    <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">tamitai147@gmail.com</p>
                         <form onSubmit={handleSubmit} className="space-y-8">
                             <div>
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
@@ -53,7 +55,7 @@ export default function Contact() {
                         <h1 className="mt-16 mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
                             Thanks for messaging!
                         </h1>
-                }
+                } */}
            </div>
         </section>
     )
