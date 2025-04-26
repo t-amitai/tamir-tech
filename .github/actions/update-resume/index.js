65e308b1f1c92a1e9a7beb005ef1a32061f1fbf7
Tamir Amitai <tamitai147@gmail.com>
2025-04-26 13:01:12 -0600

    reverts imports to cjs

--- .github/actions/update-resume/index.js
+++ .github/actions/update-resume/index.js
@@ -1,10 +1,9 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github';
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 try {
   const fileUrl = core.getInput('file-url');
   console.log(`Hello ${fileUrl}!`);
-  core.setOutput('raw-data', fileUrl);
 } catch (error) {
   core.setFailed(error.message);
 }
