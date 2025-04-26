1e88724bea652c18103536379bb2436ced7fd302
Tamir Amitai <tamitai147@gmail.com>
2025-04-26 13:09:45 -0600

    starting over

--- .github/actions/update-resume/index.js
+++
@@ -1,9 +0,0 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
-try {
-  const fileUrl = core.getInput('file-url');
-  console.log(`Hello ${fileUrl}!`);
-} catch (error) {
-  core.setFailed(error.message);
-}
