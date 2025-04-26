4aec9e9db2eee837defddc48a92543eb18e0d0dc
Tamir Amitai <tamitai147@gmail.com>
2025-04-26 14:43:19 -0600

    fixes env variable call update-resume/get.js

--- .github/actions/update-resume/get.js
+++ .github/actions/update-resume/get.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const fileUrl = core.getInput('file-url');
+  const fileUrl = process.env.FILE_URL;
   console.log(`Hello ${fileUrl}!`);
   core.setOutput('raw-data', fileUrl);
 } catch (error) {
