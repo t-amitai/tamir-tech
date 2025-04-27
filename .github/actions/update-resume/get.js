93321d33c1b3c25ac737448ecf579b829f86e680
Tamir Amitai <tamitai147@gmail.com>
2025-04-26 20:12:56 -0600

    maybe gets file

--- .github/actions/update-resume/get.js
+++ .github/actions/update-resume/get.js
@@ -1,10 +1,47 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const {google} = require('googleapis');
 
 try {
   const fileUrl = process.env.FILE_URL;
-  console.log(`Hello ${fileUrl}!`);
-  core.setOutput('raw-data', fileUrl);
+  const fileId = parseFileID(fileUrl);
+
+  const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT);
+
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: serviceAccount.client_email,
+      private_key: serviceAccount.private_key,
+    },
+    scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+  });
+
+  auth.getClient().then(authClient => {
+    let chunks = [];
+    const drive = google.drive({ version: 'v3', auth: authClient });
+    drive.files.get(
+      { fileId: fileId, alt: 'media' },
+      { responseType: 'stream' }
+    ).then(data => {
+      data
+      .on('data', chunk => chunks.push(chunk))
+      .on('end', () => {
+        const fileBuffer = Buffer.concat(chunks);
+        console.log(fileBuffer);
+      })
+      .on('error', err => reject(err));
+    });
+  });
+  
 } catch (error) {
   core.setFailed(error.message);
+}
+
+const parseFileID = (url) => {
+  const array = url.split('/');
+  for (let i = 0; i<array.length; i++) {
+    if (array[i] === 'd') {
+      return array[i+1];
+    }
+  }
 }
